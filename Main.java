93506d91350bfc6b5bc3f21c77e084f0ceb1415c
Kaique Cerri <kaiquecerrifialho1@gmail.com>
2026-09-04 10:02:11 -0300

    add .gitignore e atualização no Main

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema de projetos");
+        System.out.println("==============================");
+        System.out.println("      SISTEMA DE PROJETOS     ");
+        System.out.println("==============================");
+        System.out.println("Bem-vindo ao sistema!");
     }
 }
